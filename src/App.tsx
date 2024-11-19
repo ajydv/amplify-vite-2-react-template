005de481475b8c79e81b3f49869ac7dab5af466b
Ajay <dev.yajay@gmail.com>
2024-11-19 17:36:43 +0530

    install axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { useAuthenticator } from '@aws-amplify/ui-react';
+import axios from 'axios';
 
 const client = generateClient<Schema>();
 
@@ -24,6 +25,30 @@ function App() {
     client.models.Todo.delete({id});
   }
 
+  const handlePost = async()=>{
+    const body = { bucket_name: "demo-lambda-fun-bucket-2" };
+
+    try {
+      const response = await axios.post(
+        //'https://rs189msoe1.execute-api.eu-north-1.amazonaws.com/test-demo-2',
+        //'https://xuxtqe46q7gx3txdeeuxc5j6vq0upgxl.lambda-url.eu-north-1.on.aws/',
+        'https://dqy8074x5d.execute-api.eu-north-1.amazonaws.com/dev',
+        body,
+        {
+          headers: {
+            allowOrigins: ["*"],
+            allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+            allowHeaders: ["Content-Type"],
+          }
+        }
+      );
+      alert(`Response: ${JSON.stringify(response.data)}`);
+    } catch (error) {
+      console.error('Error making POST request:', error);
+      alert('Error making POST request');
+    }
+  }
+
   return (
     <main>
       <h1>{user?.signInDetails?.loginId}'s todos</h1>
@@ -41,6 +66,7 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={handlePost}>Post Testing</button>
       <button onClick={signOut}>Sign out</button>
     </main>
   );
